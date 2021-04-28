2497382e1ee58dfaae96546ecf784c23adb86cc1
Emil <74973350+EmiAsk@users.noreply.github.com>
2021-04-28 16:57:29 +0500

    Update main.py

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ import os
 
 
 def get_distance(p1, p2):
+    # p1 и p2 - это кортежи из двух элементов - координаты точек
     radius = 6373.0
 
     lon1 = math.radians(p1[0])
@@ -26,53 +27,32 @@ def get_distance(p1, p2):
 
 def get_geo_info(city_name, type_info='coordinates'):
     try:
+        url = "https://geocode-maps.yandex.ru/1.x/"
+        params = {
+            "apikey": "40d1649f-0493-4b70-98ba-98533de7710b",
+            'geocode': city_name,
+            'format': 'json'
+        }
+        data = requests.get(url, params).json()
         if type_info == 'country':
-            url = "https://geocode-maps.yandex.ru/1.x/"
-            params = {
-                "apikey": "40d1649f-0493-4b70-98ba-98533de7710b",
-                'geocode': city_name,
-                'format': 'json'
-            }
-            data = requests.get(url, params).json()
-            # все отличие тут, мы получаем имя страны
             return data['response']['GeoObjectCollection'][
                 'featureMember'][0]['GeoObject']['metaDataProperty'][
                 'GeocoderMetaData']['AddressDetails']['Country']['CountryName']
 
         elif type_info == 'coordinates':
-            url = "https://geocode-maps.yandex.ru/1.x/"
-            # параметры запроса
-            params = {
-                "apikey": "40d1649f-0493-4b70-98ba-98533de7710b",
-                # город, координаты которого мы ищем
-                'geocode': city_name,
-                # формат ответа от сервера, в данном случае JSON
-                'format': 'json'
-            }
-            # отправляем запрос
-            response = requests.get(url, params)
-            # получаем JSON ответа
-            json = response.json()
-            # получаем координаты города
-            # (там написаны долгота(longitude), широта(latitude) через пробел)
-            # посмотреть подробное описание JSON-ответа можно
-            # в документации по адресу https://tech.yandex.ru/maps/geocoder/
-            coordinates_str = json['response']['GeoObjectCollection'][
+            coordinates_str = data['response']['GeoObjectCollection'][
                 'featureMember'][0]['GeoObject']['Point']['pos']
-            # Превращаем string в список, так как
-            # точка - это пара двух чисел - координат
+
             long, lat = map(float, coordinates_str.split())
-            # Вернем ответ
             return long, lat
+        
     except Exception as e:
         return e
 
 
 app = Flask(__name__)
 
-# Добавляем логирование в файл.
-# Чтобы найти файл, перейдите на pythonwhere в раздел files,
-# он лежит в корневой папке
+
 logging.basicConfig(level=logging.INFO, filename='app.log',
                     format='%(asctime)s %(levelname)s %(name)s %(message)s')
 
